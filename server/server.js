d2a9762ca498c40460f0a31bc3c6e64a5d56cf38
scumworth <danmcninch@gmail.com>
2017-09-14 11:22:34 -0400

    changes process.env.API_PORT

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
-const port = process.env.API_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
